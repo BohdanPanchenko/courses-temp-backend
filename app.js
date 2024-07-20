d033dfe23b4c7e7ce9648e89c216b9ae8c933cba
Bohdan Panchenko <logitechchillstream@gmail.com>
2024-07-20 11:16:45 +0300

    bla-bla

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 const itemsRouter = require("./routes/items");
 const authRouter = require("./routes/auth");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/items", itemsRouter);
 app.use("/auth", authRouter);
